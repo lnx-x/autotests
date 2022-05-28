60b88d772ad3dc6e6499e85543df5ba5059c2029
lnx-x <106410975+lnx-x@users.noreply.github.com>
2022-05-28 06:07:22 +0300

    Update autotest_search_ozon.py

--- autotest_search_ozon.py
+++ autotest_search_ozon.py
@@ -6,7 +6,7 @@ driver = webdriver.Chrome('/home/azamat/Загрузки/chromedriver') #пут�
 driver.get('https://www.ozon.ru/') #ссылка на проверяемый сайт
 
 searchbox = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/header/div[1]/div[3]/div/form/div[2]/input[1]') #идентификатор элемента (строка поиска)
-searchbox.click()
+searchbox.click() #клик по форме
 searchbox.send_keys('Nike') #вводимое значение
 
 searchbox.send_keys(Keys.ENTER) #нажатие Enter
