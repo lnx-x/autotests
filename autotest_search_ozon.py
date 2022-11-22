a2b90bf5a38d9128dd2639ac2e1fe50449d47c2b
lnx-x <106410975+lnx-x@users.noreply.github.com>
2022-11-22 03:46:39 +0300

    Update autotest_search_ozon.py

--- autotest_search_ozon.py
+++ autotest_search_ozon.py
@@ -1,7 +1,7 @@
 from selenium import webdriver #инициализируем Селениум
 from selenium.webdriver.common.keys import Keys #инициализируем ключи Селениума
 
-driver = webdriver.Chrome('/home/azamat/Загрузки/chromedriver') #путь к Вебдрайверу
+driver = webdriver.Chrome('/home/user/download/chromedriver') #путь к Вебдрайверу
 
 driver.get('https://www.ozon.ru/') #ссылка на проверяемый сайт
 
